1cbee270c71b33228981e3b87288ec22d00e674f
dasha27atr <dasha27atr@gmail.com>
2019-09-14 23:12:14 +0300

    First commit

--- src/main/java/by/training/finalproject/entity/User.java
+++ src/main/java/by/training/finalproject/entity/User.java
@@ -1,5 +1,76 @@
 package by.training.finalproject.entity;
 
 public class User {
+    private Integer userId;
+    private String login;
+    private String password;
+    private String address;
+    private String phone;
+    private String firstName;
+    private String lastName;
+    private Integer type;
 
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
 }
